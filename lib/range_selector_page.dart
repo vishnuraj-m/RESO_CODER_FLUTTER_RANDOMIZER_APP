0668f7ef4a00c70bf1b9a862e60a3fb78707b001
vishnuacappella <acappella.studiov@gmail.com>
2022-08-07 06:08:24 +0530

    Page Setup

---
+++ lib/range_selector_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RangeSelectorPage extends StatefulWidget {
+  const RangeSelectorPage({super.key});
+
+  @override
+  State<RangeSelectorPage> createState() => _RangeSelectorPageState();
+}
+
+class _RangeSelectorPageState extends State<RangeSelectorPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
