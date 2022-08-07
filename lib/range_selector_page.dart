9fd8a5cfb2138012e6be4f749bc3ab40ee328b81
vishnuacappella <acappella.studiov@gmail.com>
2022-08-07 08:24:42 +0530

    Creating the Input Form UI

--- lib/range_selector_page.dart
+++ lib/range_selector_page.dart
@@ -15,7 +15,37 @@ class _RangeSelectorPageState extends State<RangeSelectorPage> {
         title: const Text('Select Range'),
       ),
       body: Form(
-        child: Container(),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Minimum',
+                ),
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: false,
+                  signed: true,
+                ),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Maximum',
+                ),
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: false,
+                  signed: true,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.arrow_forward),
