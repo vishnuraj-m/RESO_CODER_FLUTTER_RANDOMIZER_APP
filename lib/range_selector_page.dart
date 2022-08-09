09cb1e7676eb0706c4ef44fb142c02c38822d499
vishnuacappella <acappella.studiov@gmail.com>
2022-08-09 11:42:15 +0530

    Providing the ChangeNotifier Around the App Using Provider

--- lib/range_selector_page.dart
+++ lib/range_selector_page.dart
@@ -1,26 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_foundations/randomizer_page.dart';
 import 'package:flutter_foundations/range_selector_form.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 
-class RangeSelectorPage extends HookWidget {
+class RangeSelectorPage extends StatelessWidget {
   final formKey = GlobalKey<FormState>();
 
   RangeSelectorPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final min = useState<int>(0);
-    final max = useState<int>(0);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Select Range'),
       ),
       body: RangeSelectorForm(
         formKey: formKey,
-        minValueSetter: (value) => min.value = value,
-        maxValueSetter: (value) => max.value = value,
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.arrow_forward),
@@ -30,10 +24,7 @@ class RangeSelectorPage extends HookWidget {
           }
           Navigator.of(context).push(
             MaterialPageRoute(
-              builder: (context) => RandomizerPage(
-                min: min.value,
-                max: max.value,
-              ),
+              builder: (context) => RandomizerPage(),
             ),
           );
         },
