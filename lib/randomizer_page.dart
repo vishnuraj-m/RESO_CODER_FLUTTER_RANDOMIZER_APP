c486251bf6b6cf719d0aba9470fae2d811cf362d
vishnuacappella <acappella.studiov@gmail.com>
2022-08-09 05:44:22 +0530

    Implementing the Randomizer Page

--- lib/randomizer_page.dart
+++ lib/randomizer_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class RandomizerPage extends StatefulWidget {
@@ -13,6 +15,9 @@ class RandomizerPage extends StatefulWidget {
 }
 
 class _RandomizerPageState extends State<RandomizerPage> {
+  int? _generatedNumber;
+  final randomGenerator = Random();
+
   @override
   Widget build(BuildContext context) {
     widget.min;
@@ -20,6 +25,22 @@ class _RandomizerPageState extends State<RandomizerPage> {
       appBar: AppBar(
         title: const Text('Randomizer'),
       ),
+      body: Center(
+        child: Text(
+          _generatedNumber?.toString() ?? 'Generate a Number',
+          style: const TextStyle(fontSize: 42),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text('Generate'),
+        onPressed: () {
+          setState(() {
+            _generatedNumber = widget.min +
+                randomGenerator.nextInt(widget.max + 1 - widget.min);
+          });
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
